448fb96af95fe49f55dcca1637c3d9c3dac2e3b1
Vjacheslav Borisov <slavb@iconicompany.com>
2023-03-07 09:36:20 +0400

    BASEPATH

--- next.config.js
+++ next.config.js
@@ -1,5 +1,7 @@
-const basePath = '/icompanytest';
-module.exports = {
-  basePath,
-  assetPrefix: basePath,
-};
+const nextConfig = {};
+if (process.env.BASEPATH) {
+  nextConfig.basePath = process.env.BASEPATH;
+  nextConfig.assetPrefix = process.env.BASEPATH;
+}
+
+module.exports = nextConfig;
